7e00ed0cb3f997b36695dcbace512d2a10f60aba
maheenfayyaz <princessfayyaz7@gmail.com>
2025-10-05 21:14:03 +0500

    Hackaton Successfully Completed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL)
+  }
 })
